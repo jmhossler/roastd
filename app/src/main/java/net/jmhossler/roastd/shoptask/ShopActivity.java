f97f058d24f3a8f69ac2dcaeeba2eaccde1755d1
John Hossler <john.m.hossler@gmail.com>
2018-04-25 12:57:51 -0500

    Added some baseline stuff. Nothing connected graphically yet, though

--- app/src/main/java/net/jmhossler/roastd/shoptask/ShopActivity.java
+++ app/src/main/java/net/jmhossler/roastd/shoptask/ShopActivity.java
@@ -4,14 +4,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import net.jmhossler.roastd.R;
+import net.jmhossler.roastd.data.bean.FirebaseRTBeanRepository;
+import net.jmhossler.roastd.data.drink.FirebaseRTDrinkRepository;
+import net.jmhossler.roastd.data.searchableItem.FirebaseRTSearchableItemRepository;
+import net.jmhossler.roastd.data.searchableItem.SearchableItem;
+import net.jmhossler.roastd.data.shop.FirebaseRTShopRepository;
+import net.jmhossler.roastd.data.user.FirebaseRTUserRepository;
 
-public class ShopActivity extends AppCompatActivity {
+import java.util.List;
 
-  private ShopPresenter mShopPresenter;
+public class ShopActivity extends AppCompatActivity implements ShopContract.View {
+
+  private ShopContract.Presenter mShopPresenter;
+
+  @Override
+  public void setPresenter(ShopContract.Presenter shopPresenter) {
+    mShopPresenter = shopPresenter;
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_shop);
+    String shopId = getIntent().getStringExtra("SHOP_ID");
+
+
+
+    mShopPresenter = new ShopPresenter(this, shopId, FirebaseRTUserRepository.getsInstance(),
+      FirebaseRTShopRepository.getInstance(), FirebaseRTSearchableItemRepository.getInstance());
+  }
+
+  @Override
+  public void displayName(String name) {
+
+  }
+
+  @Override
+  public void displayDescription(String description) {
+
+  }
+
+  @Override
+  public void displayAddress(String address) {
+
+  }
+
+  @Override
+  public void displayMapsUrl(String url) {
+
+  }
+
+  @Override
+  public void displayConsumeables(List<SearchableItem> items) {
+
   }
 }
